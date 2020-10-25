3d4a1e4105215ffcbf18ea20f803010b4800492f
Jaeiya <exanix@gmail.com>
2020-10-24 17:50:14 -0700

    PageBuilder{}: Add _loadFiles()
    This also adds other supporting methods required for the
    `_loadFiles()`.

--- lib/page_builder.ts
+++ lib/page_builder.ts
@@ -1,27 +1,70 @@
 import { promises, existsSync } from 'fs';
-import fm from 'front-matter';
+import frontMatter, { FrontMatterResult } from 'front-matter';
 
 
+
+interface MDFormat {
+  title: string;
+  author: string;
+  date?: string;
+}
 
 
 export class PageBuilder {
 
-  private _paths;
+  private _dirs;
   /** Promisified File System */
   private _pfs = promises;
+  private _fileData: Map<string, FrontMatterResult<MDFormat>[]> = new Map();
+  private _oldFileData: Map<string, MDFormat[]> = new Map();
 
-  get arePathsValid() {
-    for (const p of this._paths) {
+  get areDirsValid() {
+    for (const p of this._dirs) {
       if (!existsSync(p)) return false;
     }
     return true;
   }
 
 
-  constructor(paths: string[]) {
-    this._paths = paths;
-    if (!this._paths.length) throw Error('Path configuration is EMPTY.');
-    if (!this.arePathsValid) throw Error('One or more paths do NOT exist.');
+  constructor(dirs: string[], public onReady: (err: Error|null) => void) {
+    this._dirs = dirs;
+    this._loadFiles();
    }
+
+  async _loadFiles() {
+    try {
+      if (!this._dirs.length) throw Error('Directory configuration is EMPTY.');
+      if (!this.areDirsValid) throw Error('One or more paths do NOT exist.')
+      ;
+      for (const dir of this._dirs) {
+        const fileNames = await this._pfs.readdir(dir);
+        const filePaths = fileNames.map(name => `${dir}/${name}`);
+        const filesData = await this.parseFiles(filePaths);
+        this._fileData.set(dir, filesData);
+      }
+      this.onReady(null);
+    }
+    catch (err) { this.onReady(err); }
+  }
+
+  async parseFiles(filePaths: string[]) {
+    const fileStrings = await this._readAllFiles(filePaths);
+    return fileStrings.map(data => {
+      if (!frontMatter.test(data))
+        throw Error(
+          `Invalid or Missing front matter \nContents: "${data.substr(0, 30)}"`
+      );
+      return frontMatter<MDFormat>(data);
+    });
+  }
+
+  private async _readAllFiles(filePaths: string[]) {
+    const fileData: string[] = [];
+    for (const path of filePaths) {
+      const data = (await this._pfs.readFile(path)).toString('utf-8');
+      fileData.push(data);
+    }
+    return fileData;
+  }
 
 }
