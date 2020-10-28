e2de2137f8954459899665551e3f63b91eed2b98
Jaeiya <exanix@gmail.com>
2020-10-28 11:26:20 -0700

    Add directory property tests and update directory references

--- specs/page_builder.test.ts
+++ specs/page_builder.test.ts
@@ -4,6 +4,8 @@ import smap from 'source-map-support';
 import { writeFile } from 'fs';
 import importFresh from 'import-fresh';
 import testAddPages from './mocks/test_add_page/test_add_page.json';
+import { sep, basename as pathBasename } from 'path';
+import fs from 'fs';
 
 smap.install();
 
@@ -64,51 +66,86 @@ tape('PageBuilder{}', t => {
   });
   t.test('constructor() populates internal map with valid Pages.', t => {
     const testFolder = `${mockFolder}/test_valid_directory`;
-    t.plan(1); const pb = new PageBuilder([testFolder], (err) => {
+    t.plan(2); const pb = new PageBuilder([testFolder], (err) => {
+      t.is(err, null, 'error should be null');
       const values = Array.from(pb.pages.values())[0].map(val => val.title);
       t.same(values, ['testing the files', 'what is a love', 'what is a silence']);
+    });
+  });
+
+  t.test('get dirs: return absolute paths of internal directories.', t => {
+    const rootDir = `${mockFolder}/test_multiple_directories`;
+    const testFolders = [
+      `${rootDir}/one`,
+      `${rootDir}/two`,
+      `${rootDir}/three`
+    ];
+    t.plan(1); const pb = new PageBuilder(testFolders, async (err) => {
+      const isAbsPath = (dir: string) => (!!~dir.indexOf(`:${sep}`));
+      t.ok(pb.dirs.every(isAbsPath), 'contains root file directory');
+    });
+  });
+
+  t.test('get shortDirs: return short paths of internal directories.', t => {
+    const rootDir = `${mockFolder}/test_multiple_directories`;
+    const testFolders = [
+      `${rootDir}/one`,
+      `${rootDir}/two`,
+      `${rootDir}/three`
+    ];
+    t.plan(1); const pb = new PageBuilder(testFolders, async (err) => {
+      const isShortPath =
+        (dir: string) => (dir == `test_multiple_directories/${pathBasename(dir)}`)
+      ;
+      t.ok(pb.shortDirs.every(isShortPath));
     });
   });
 
   t.test('updatePages() adds pages if they do not exist in JSON file.', t => {
     t.plan(3); const pb = new PageBuilder([`${mockFolder}/test_add_page`], async (err) => {
       if (err) throw err;
-      const getPages = () => importFresh('./mocks/test_add_page/test_add_page.json') as Promise<typeof testAddPages>;
-      const oldPages = (await getPages());
-      const pageExistsTest1 = oldPages.find(page => page.title == 'test adding this page');
-      await pb.updatePages();
-      const pageExistsTest2 = (await getPages()).find(page => page.title == 'test adding this page');
-      const pagesFromMap = pb.pages.get(`${mockFolder}/test_add_page`)
+      const getPages = () =>
+        importFresh(`${pb.dirs[0]}/test_add_page.json`) as Promise<typeof testAddPages>
       ;
-      t.is(pageExistsTest1,    undefined,    'page does not already exist');
-      t.isNot(pageExistsTest2, undefined,    'page is saved after update');
-      t.same(await getPages(), pagesFromMap, 'JSON file matches internal page map')
+      const oldPages = (await getPages());
+      const oldPage  = oldPages.find(page => page.title == 'test adding this page');
+      await pb.updatePages();
+      const updPages     = await getPages();
+      const addedPage    = updPages.find(page => page.title == 'test adding this page');
+      const pagesFromMap = pb.pages.get(pb.dirs[0])
+      ;
+      t.is(     oldPage,    undefined, 'page does not already exist');
+      t.isNot(addedPage,    undefined, 'page is saved after update');
+      t.same(  updPages, pagesFromMap, 'JSON file matches internal page map')
       ;
       // Cleanup
       writeFile(
-        `${mockFolder}/test_add_page/test_add_page.json`,
+        `${pb.dirs[0]}/test_add_page.json`,
         JSON.stringify(oldPages, null, 2),
         () => {return;}
       );
     });
   });
-  t.test('updatePages() deletes pages if they do not exist in pages map.', t => {
+  t.test('updatePages() deletes pages if they do not exist in JSON file.', t => {
     t.plan(3); const pb = new PageBuilder([`${mockFolder}/test_delete_page`], async (err) => {
       if (err) throw err;
-      const getPages = () => importFresh('./mocks/test_delete_page/test_delete_page.json') as Promise<typeof testAddPages>;
-      const oldPages = (await getPages());
-      const pageExistsTest1 = oldPages.find(page => page.title == 'test to delete this page');
-      await pb.updatePages(); // file does not exist in directory
-      const pageExistsTest2 = (await getPages()).find(page => page.title == 'test to delete this page');
-      const pagesFromMap = pb.pages.get(`${mockFolder}/test_delete_page`)
+      const getPages = () =>
+        importFresh(`${pb.dirs[0]}/test_delete_page.json`) as Promise<typeof testAddPages>
       ;
-      t.isNot(pageExistsTest1,    undefined, 'page exists to delete');
-      t.is(pageExistsTest2,       undefined, 'page is deleted after update');
-      t.same(await getPages(), pagesFromMap, 'JSON file matches internal page map')
+      const oldPages = await getPages();
+      const oldPage  = oldPages.find(page => page.title == 'test to delete this page');
+      await pb.updatePages(); // file does not exist in directory
+      const updPages     = await getPages();
+      const deletedPage  = updPages.find(page => page.title == 'test to delete this page');
+      const pagesFromMap = pb.pages.get(pb.dirs[0])
+      ;
+      t.isNot( oldPage,    undefined, 'page exists to delete');
+      t.is(deletedPage,    undefined, 'page is deleted after update');
+      t.same( updPages, pagesFromMap, 'JSON file matches internal page map')
       ;
       // Cleanup
       writeFile(
-        `${mockFolder}/test_delete_page/test_delete_page.json`,
+        `${pb.dirs[0]}/test_delete_page.json`,
         JSON.stringify(oldPages, null, 2),
         () => {return;}
       );
@@ -118,23 +155,23 @@ tape('PageBuilder{}', t => {
     t.plan(3); const pb = new PageBuilder([`${mockFolder}/test_change_page`], async (err) => {
       if (err) throw err;
       const getPages = () =>
-        importFresh('./mocks/test_change_page/test_change_page.json') as Promise<typeof testAddPages>
+        importFresh(`${pb.dirs[0]}/test_change_page.json`) as Promise<typeof testAddPages>
       ;
       const oldPages = await getPages();
       const oldPage  = oldPages.find(page => page.title == 'page that changes');
       await pb.updatePages(); // file is changed in directory
       const updPages     = await getPages();
-      const newPage      = updPages.find(page => page.title == 'page that changes');
-      const pagesFromMap = pb.pages.get(`${mockFolder}/test_change_page`);
+      const updPage      = updPages.find(page => page.title == 'page that changes');
+      const pagesFromMap = pb.pages.get(pb.dirs[0]);
       const changedStr   = 'This content is now changed.'
       ;
       t.isNot(      oldPage,    undefined, 'page exists to change');
-      t.is(newPage?.content,   changedStr, 'page content is changed after update');
+      t.is(updPage?.content,   changedStr, 'page content is changed after update');
       t.same(      updPages, pagesFromMap, 'JSON file matches internal page map')
       ;
       // Cleanup
       writeFile(
-        `${mockFolder}/test_change_page/test_change_page.json`,
+        `${pb.dirs[0]}/test_change_page.json`,
         JSON.stringify(oldPages, null, 2),
         () => {return;}
       );
