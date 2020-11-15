99918bfd4ed186684c83ee8e9461eb28cdeae92a
Jaeiya <exanix@gmail.com>
2020-11-15 15:37:09 -0800

    Create self-contained export
    This package is now setup to be used inside of a personalized
    script, whether a dev wants custom logic or they can execute
    the default export as-is.

--- lib/index.ts
+++ lib/index.ts
@@ -1,16 +1,18 @@
 import { MDPageBundler } from "./core/md_page_bundler";
-import config from './config.json';
 import bunyan from 'bunyan';
 
 const log = bunyan.createLogger({
   name: 'entry'
 });
 
-const pb = new MDPageBundler(config.paths, async (err) => {
-  if (err) {
-    log.error(err);
-    process.exit(); // All errors are fatal
-  }
-  try         { await pb.processPages('html'); }
-  catch (err) { log.error(err);         }
+
+export default (function(dirs: string[], outType: 'plain'|'html') {
+  const pb = new MDPageBundler(dirs, async (err) => {
+    if (err) {
+      log.error(err);
+      process.exit(); // All errors are fatal
+    }
+    try         { await pb.processPages(outType); }
+    catch (err) { log.error(err); }
+  });
 });
