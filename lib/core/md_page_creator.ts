b5b65f7d7df363045c47bc2a6a6967279ebb9867
Jaeiya <exanix@gmail.com>
2020-12-16 13:36:11 -0800

    Allow custom front matter properties

--- lib/core/md_page_creator.ts
+++ lib/core/md_page_creator.ts
@@ -5,6 +5,7 @@ import { sep as pathSep} from 'path';
 
 
 type MDPage = [title: string, content: string];
+type FrontMatterObj = { [key: string]: string|undefined };
 
 
 
@@ -15,27 +16,32 @@ export default class MDPageCreator {
 
 
   public createPages(pages: Page[]) {
-    const mdPages = pages.map(this.createMDPage);
+    const mdPages = pages.map(page => this.createMDPage(page));
     return this._savePages(mdPages);
   }
 
 
   private createMDPage(page: Page): MDPage {
-    return (
-[page.title,
-`---
-title: ${page.title}
-author: ${page.author}
-date: ${page.date}
----
-${page.content}`]
-    );
+    const {title, author, date, content, ...custom } = page;
+    const fmStr = this.createFrontMatter({ title, author, date, ...custom });
+    return [page.title, `${fmStr}\n${content}`];
   }
 
+
+  private createFrontMatter(fm: FrontMatterObj) {
+    const fence = '---';
+    let fmStr = fence
+    ;
+    for (const key in fm) fmStr += `\n${key}: ${fm[key]}`;
+    fmStr += `\n${fence}`;
+    return fmStr;
+  }
+
+
   private async _savePages(pages: MDPage[]) {
-    for (const p of pages) {
+    for (const page of pages) {
       await fsPromises.writeFile(
-        `${this._dir}${pathSep}${p[0]}.md`, p[1], 'utf8'
+        `${this._dir}${pathSep}${page[0]}.md`, page[1], 'utf8'
       );
     }
   }
